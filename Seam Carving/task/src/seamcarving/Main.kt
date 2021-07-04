713f9dfa7b0571e20d240be4c78d4939af8a1988
Marc Forrer <marc.forrer@bluewin.ch>
2021-07-04 22:58:58 +0200

    finished stage 3

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -5,20 +5,66 @@ import java.awt.image.BufferedImage
 import java.io.File
 import java.io.FileOutputStream
 import javax.imageio.ImageIO
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
     val inFile = args[args.indexOf("-in") + 1]
     val outFile = args[args.indexOf("-out") + 1]
     val inImage = ImageIO.read(File(inFile))
     val outImage = BufferedImage(inImage.width, inImage.height, BufferedImage.TYPE_INT_RGB)
+    val maxEnergyValue = getMaxEnergy(inImage)
     for (x in 0 until inImage.width) {
         for (y in 0 until inImage.height) {
-            val color = Color(inImage.getRGB(x, y))
-            val negativeColor = Color(255 - color.red, 255 - color.green, 255 - color.blue)
-            outImage.setRGB(x, y, negativeColor.rgb)
+            val pixelEnergy = getPixelEnergy(inImage, x, y)
+            val intensity = (255.0 * pixelEnergy / maxEnergyValue).toInt()
+            val intensityColor = Color(intensity, intensity, intensity)
+            outImage.setRGB(x, y, intensityColor.rgb)
         }
     }
     ImageIO.write(outImage, "png", File(outFile))
+}
+
+fun getMaxEnergy(image: BufferedImage): Double {
+    var highestEnergy = 0.0
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            val energy = getPixelEnergy(image, x, y)
+            if (energy > highestEnergy) {
+                highestEnergy = energy
+            }
+        }
+    }
+    return highestEnergy
+}
+
+fun getPixelEnergy(image: BufferedImage, x: Int, y: Int): Double {
+    val horizontalDelta = getHorizontalDelta(image, x, y)
+    val verticalDelta = getVerticalDelta(image, x, y)
+    return sqrt(horizontalDelta + verticalDelta)
+}
+
+fun getHorizontalDelta(image: BufferedImage, x: Int, y: Int): Double {
+    val actualX = when (x) { 0 -> 1; image.width - 1 -> image.width - 2; else -> x }
+    val before = Color(image.getRGB(actualX - 1, y))
+    val after = Color(image.getRGB(actualX + 1, y))
+    return (before.red - after.red).toDouble().pow(2) +
+            (before.green - after.green).toDouble().pow(2) +
+            (before.blue - after.blue).toDouble().pow(2)
+}
+
+fun getVerticalDelta(image: BufferedImage, x: Int, y: Int): Double {
+    val actualY = when (y) { 0 -> 1; image.height - 1 -> image.height - 2; else -> y }
+    val before = Color(image.getRGB(x, actualY - 1))
+    val after = Color(image.getRGB(x, actualY + 1))
+    return (before.red - after.red).toDouble().pow(2) +
+            (before.green - after.green).toDouble().pow(2) +
+            (before.blue - after.blue).toDouble().pow(2)
+}
+
+fun getNegativeColor(image: BufferedImage, x: Int, y: Int): Color {
+    val color = Color(image.getRGB(x, y))
+    return Color(255 - color.red, 255 - color.green, 255 - color.blue)
 }
 
 fun createRectangle() {
